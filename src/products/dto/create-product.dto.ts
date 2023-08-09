6d3474574a54feda8db89247b54791184f50f981
Matias Lenci <matiaslenci@gmail.com>
2023-08-08 22:45:16 -0300

    logica validacion de create dto product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,42 @@
-export class CreateProductDto {}
+import {
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+  IsInt,
+  IsArray,
+  IsIn,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  slug?: string;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsString()
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
